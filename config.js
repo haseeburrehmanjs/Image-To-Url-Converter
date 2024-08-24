da1f3cfd0f53e6f57c5efe3099a4b23a7bf7830f
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-08-25 00:22:49 +0500

    modified work

---
+++ config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAH4gJYU_M1-jY5zVbiuV2x8fw60f0gwNU",
+    authDomain: "image-to-url-converter-9483c.firebaseapp.com",
+    projectId: "image-to-url-converter-9483c",
+    storageBucket: "image-to-url-converter-9483c.appspot.com",
+    messagingSenderId: "765703459210",
+    appId: "1:765703459210:web:fc8a1c93e5558d7e34b873",
+    measurementId: "G-XVTQN9V60L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ main.js
@@ -0,0 +1,33 @@
+import {
+    uploadBytes,
+    getDownloadURL,
+    ref,
+    getStorage
+} from "https://www.gstatic.com/firebasejs/10.13.0/firebase-storage.js";
+
+let storage = getStorage()
+
+let form = document.querySelector('#form')
+let myfile = document.querySelector('#myFile')
+let email = document.querySelector('#email')
+
+form.addEventListener('submit', async event => {
+    event.preventDefault()
+    let file = myfile.files[0]
+    let url = await uploadFile(file, email.value)
+    console.log(url);
+})
+
+
+
+async function uploadFile(file, userEmail) {
+    const storageRef = ref(storage, userEmail);
+    try {
+        const uploadImg = await uploadBytes(storageRef, file);
+        const url = await getDownloadURL(uploadImg.ref);
+        return url;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
